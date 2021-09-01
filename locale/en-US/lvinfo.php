06460e80c89863295ccd92a4bf6fd0ac3fe24c5b
Nikolaus Krondraf <krondraf@technikum-wien.at>
2021-09-01 10:37:02 +0200

    Phrasen für LVInfo Addon ergänzt

---
+++ locale/en-US/lvinfo.php
@@ -0,0 +1,3 @@
+<?php
+$this->phrasen['lvinfo/internerHinweis_klvt']='Wird ins kommentierte LV-Verzeichnis und auf die Homepage übertragen.';
+$this->phrasen['lvinfo/internerHinweis_lit']='max. 5 Titel';
