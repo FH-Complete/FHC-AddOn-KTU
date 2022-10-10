07db695cede5a307b2bc70ff6b57c1b7e5496df9
Nikolaus Krondraf <krondraf@technikum-wien.at>
2022-10-10 13:43:30 +0200

    add phrase

--- locale/de-AT/lvinfo.php
+++ locale/de-AT/lvinfo.php
@@ -1,3 +1,4 @@
 <?php
 $this->phrasen['lvinfo/internerHinweis_klvt']='Wird ins kommentierte LV-Verzeichnis und auf die Homepage übertragen.';
 $this->phrasen['lvinfo/internerHinweis_lit']='max. 5 Titel';
+$this->phrasen['lvinfo/freigabeberechtigt']='Bei Fragen wenden Sie sich an:';
